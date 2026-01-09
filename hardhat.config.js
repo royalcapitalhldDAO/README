9141915613948050029de5265a97750203b686b3
royalcapitalhldDAO <royalcapitalhldDAO@proton.me>
2026-01-09 01:36:40 -0500

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,26 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.DEPLOYER_PRIVATE_KEY]
+    }
+  }
+};
+
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.DEPLOYER_PRIVATE_KEY]
+    }
+  }
+};
